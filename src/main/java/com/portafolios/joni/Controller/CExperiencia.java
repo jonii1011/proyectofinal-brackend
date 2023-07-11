3a261359ac228430efd89c9d7f480d2c9f159c0f
jonii1011 <jonii10lea@gmail.com>
2023-07-11 19:46:46 -0300

    .

--- src/main/java/com/portafolios/joni/Controller/CExperiencia.java
+++ src/main/java/com/portafolios/joni/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("explab")
+@RequestMapping("/explab")
 @CrossOrigin(origins = {"https://proyectofinal-frontend-5f110.web.app", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
